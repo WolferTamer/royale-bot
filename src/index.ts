a3b1394f8e9f3d427a098a5213540f23a0b19ea6
ImWolfer <calebbcassin@gmail.com>
2025-05-22 16:27:44 -0700

    Init Node project with typescript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import {Client} from 'discord.js'
+
+const client = new Client({
+    'intents':['Guilds','GuildMessages','GuildMembers','MessageContent']
+})
+
+client.on('ready', (c) => {
+    console.log(`${c.user.username} is online.`);
+})
+
+console.log(process.env.DISCORD_TOKEN)
